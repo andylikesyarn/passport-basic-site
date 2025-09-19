baf005983689df0fc90563bff926a4672caa0d30
Andy Stowers <andystowers@MacBook-Pro.local>
2025-09-19 20:21:52 -0300

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Home from './Home';
-import New from './pages/New';
-import Renew from './pages/Renew';
-import Change from './pages/Change';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./Home";
+import New from "./pages/New";
+import Renew from "./pages/Renew";
+import Change from "./pages/Change";
+import gftoImage from "./assets/GTFO!.png";
+import "./App.css";
 
 function App() {
   return (
@@ -12,22 +13,35 @@ function App() {
         {/* Navigation */}
         <nav className="navbar">
           <div className="nav-container">
-            <Link to="/" className="nav-logo">
-              QTS Passport Guide
-            </Link>
+            <div className="nav-left">
+              <Link to="/" className="nav-logo-link">
+                <img src={gftoImage} alt="GTFO" className="nav-logo-image" />
+              </Link>
+              <Link to="/" className="nav-logo">
+                QTS Passport Guide
+              </Link>
+            </div>
             <div className="nav-menu">
               <Link to="/" className="nav-link">
                 Home
               </Link>
-              <Link to="/new" className="nav-link">
-                New Passport
-              </Link>
-              <Link to="/renew" className="nav-link">
-                Renew Passport
-              </Link>
-              <Link to="/change" className="nav-link">
-                Change/Correct
-              </Link>
+              <div className="dropdown">
+                <span className="dropdown-trigger">
+                  Application Types
+                  <span className="dropdown-arrow">▼</span>
+                </span>
+                <div className="dropdown-content">
+                  <Link to="/new" className="dropdown-link">
+                    First-time Passport
+                  </Link>
+                  <Link to="/renew" className="dropdown-link">
+                    Renew Passport
+                  </Link>
+                  <Link to="/change" className="dropdown-link">
+                    Change/Correct Passport
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
         </nav>
--- src/pages/Renew.jsx
+++ src/pages/Renew.jsx
@@ -7,20 +7,27 @@ function Renew() {
 
   return (
     <>
-      <div className="header-image">
-        <img src={gftoImage} alt="GFTO" className="gfto-logo" />
-      </div>
       <h1>Need a Passport? Here's what to do!</h1>
-      <div className="cards-container">
+      <div className="change-cards-container">
         <div className="card card2">
           <div className="card-content">
             <div className="main-text">
               If you've <span>gotten a passport before as an adult</span>
             </div>
             , you'll need to fill out the
-            <a href="https://eforms.state.gov/Forms/ds86_pdf.PDF">DS-86</a>
+            <a
+              href="https://eforms.state.gov/Forms/ds82_pdf.PDF"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              DS-82
+            </a>
             If you want to use the form filler to fill it out online, use{" "}
-            <a href="https://pptform.state.gov/PassportWizardMain.aspx">
+            <a
+              href="https://pptform.state.gov/PassportWizardMain.aspx"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               this link
             </a>
           </div>
@@ -28,55 +35,15 @@ function Renew() {
             {" "}
             <span>**RECOMMENDED**</span> Ideally, you'll actually want to apply
             online{" "}
-            <a href="https://travel.state.gov/content/travel/en/passports/have-passport/renew-online.html">
+            <a
+              href="https://travel.state.gov/content/travel/en/passports/have-passport/renew-online.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               here
             </a>
             . There are some restrictions, but I recommend applying online if
             you can.
-          </div>
-          <div className="card-arrow">↓</div>
-        </div>
-
-        <div className="card card1">
-          <div className="card-content">
-            <div className="main-text">
-              If you've <span> NEVER gotten a passport before as an adult</span>
-            </div>
-            , you'll need to fill out the <span>DS-11</span> If you'd rather
-            print it out and fill it out by hand, print this form:{" "}
-            <a href="https://eforms.state.gov/Forms/ds11_pdf.PDF">DS-11</a>
-            <br></br>
-            If you want to use the form filler to fill it out online, use{" "}
-            <a href="https://pptform.state.gov/PassportWizardMain.aspx">
-              this link
-              <span>**RECOMMENDED**</span>
-            </a>
-          </div>
-          <div className="card-arrow">↓</div>
-        </div>
-
-        <div className="card card5">
-          <div className="card-content">
-            {" "}
-            <span className="main-text">
-              {" "}
-              If you've gotten a passport before as an adult but you're changing
-              your name or gender marker
-            </span>
-            <p>
-              ...you'll need to fill out the <span>DS-11</span>. If you'd rather
-              print it out and fill it out by hand, print this form:{" "}
-              <a href="https://eforms.state.gov/Forms/ds11_pdf.PDF">DS-11</a>
-            </p>
-            <br></br>
-            <p>
-              {" "}
-              If you want to use the form filler to fill it out online, use{" "}
-              <a href="https://pptform.state.gov/PassportWizardMain.aspx">
-                this link
-                <span>**RECOMMENDED**</span>
-              </a>
-            </p>
           </div>
           <div className="card-arrow">↓</div>
         </div>
@@ -97,65 +64,16 @@ function Renew() {
           <div className="card-arrow">↓</div>
         </div>
 
-        <div className="card card3">
-          <div className="card-content">
-            Next, you'll need to gather your{" "}
-            <strong>supporting documents</strong>:
-            <ul>
-              <li>
-                Proof of U.S. citizenship (
-                <a
-                  href="https://docs.google.com/spreadsheets/d/1l3xQERijEXlgj_w8g6G62krdeLwqX2lpt09o35Sd3sI/edit?gid=857010838#gid=857010838"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  birth certificate
-                </a>
-                , naturalization certificate, etc.)
-              </li>
-              <li>
-                Photo identification (driver's license, military ID, etc.)
-              </li>
-              <li>Passport photo (2" x 2")</li>
-              <li>Payment for fees</li>
-            </ul>
-          </div>
-          <div className="card-arrow">↓</div>
-        </div>
-
-        <div className="card card7">
-          <div className="card-content">
-            Next, you'll need to gather your{" "}
-            <strong>supporting documents</strong>:
-            <ul>
-              <li>
-                Proof of U.S. citizenship (
-                <a
-                  href="https://docs.google.com/spreadsheets/d/1l3xQERijEXlgj_w8g6G62krdeLwqX2lpt09o35Sd3sI/edit?gid=857010838#gid=857010838"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  birth certificate
-                </a>
-                , naturalization certificate, etc.)
-              </li>
-              <li>
-                Photo identification (driver's license, military ID, etc.)
-              </li>
-              <li>Passport photo (2" x 2")</li>
-              <li>Payment for fees</li>
-              <li>Supplementary documents (see below)</li>
-            </ul>
-          </div>
-          <div className="card-arrow">↓</div>
-        </div>
-
         <div className="card card6">
           <div className="card-content">
             <strong>Additional documents some people might need:</strong>
             <ul>
               <li>
-                <a href="https://docs.google.com/spreadsheets/d/1chrp-sViyX6zzisFwULIWmFz1EU_lF9_7htXBLpmHo0/edit?gid=246470848#gid=246470848">
+                <a
+                  href="https://docs.google.com/spreadsheets/d/1chrp-sViyX6zzisFwULIWmFz1EU_lF9_7htXBLpmHo0/edit?gid=246470848#gid=246470848"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   Request marriage certificate
                 </a>
               </li>
@@ -169,12 +87,20 @@ function Renew() {
                 </a>
               </li>
               <li>
-                <a href="https://travel.state.gov/content/travel/en/passports/have-passport/change-correct.html">
+                <a
+                  href="https://travel.state.gov/content/travel/en/passports/have-passport/change-correct.html"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   Name change request
                 </a>
               </li>
               <li>
-                <a href="https://travel.state.gov/content/travel/en/passports/passport-help/sex-marker.html">
+                <a
+                  href="https://travel.state.gov/content/travel/en/passports/passport-help/sex-marker.html"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   Gender marker change request
                 </a>
               </li>
